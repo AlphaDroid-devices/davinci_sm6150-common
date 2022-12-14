f68582521d9ed85a5f369ef53714d2445c045eff
helliscloser <mrumais@gmail.com>
2022-12-14 15:35:27 +0530

    sm6150-common: Parts: Change package name for merging to SettingsLibWidget
    Change required since Dec 2022 QPR patch
    
    Signed-off-by: helliscloser <mrumais@gmail.com>
    Co-authored-by: Hung Phan <phandinhhungvp2001@gmail.com>

--- parts/src/org/lineageos/settings/vibrator/VibratorSettingsActivity.java
+++ parts/src/org/lineageos/settings/vibrator/VibratorSettingsActivity.java
@@ -2,7 +2,7 @@ package org.lineageos.settings.vibrator;
 
 import android.os.Bundle;
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class VibratorSettingsActivity extends CollapsingToolbarBaseActivity {
 
