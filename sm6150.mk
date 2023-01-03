736bd76e0cf5d2743c80f123dccc23c995a9f872
helliscloser <mrumais@gmail.com>
2023-01-03 10:40:23 +0530

    RiceDroid! Bring-Up
    Signed-off-by: helliscloser <mrumais@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -276,7 +276,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-cherish
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
