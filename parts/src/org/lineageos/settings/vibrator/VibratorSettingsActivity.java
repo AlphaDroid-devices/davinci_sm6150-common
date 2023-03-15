Spark-Tmp/android_device_xiaomi_sm6150-common

sm6150-common: parts: Add vibration control
Laker87: Implementation
markkey014: Adapted to S and added IT translation

signed off by: Mark Rogacion <khevinrogacion@gmail.com>

Parts: Add vibration control

Laker87: Implementation
markkey014: Adapted to S and added IT translation

signed off by: Mark Rogacion <khevinrogacion@gmail.com>

Add vibration control

Co-Authored-By: Laker <61315154+laker87@users.noreply.github.com>

parts/src/org/lineageos/settings/vibrator/VibratorSettingsActivity.java
@@ -1,0 +1,18 @@
+package org.lineageos.settings.vibrator;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class VibratorSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG = "VibratorSettingsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new VibratorSettings(), TAG)
+        .commit();
+    }
+}

AlphaDroid-devices/davinci_sm6150-common

Signed-off-by: helliscloser <mrumais@gmail.com>

parts/src/org/lineageos/settings/vibrator/VibratorSettingsActivity.java
@@ -1,0 +1,18 @@
+package org.lineageos.settings.vibrator;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class VibratorSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG = "VibratorSettingsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new VibratorSettings(), TAG)
+        .commit();
+    }
+}